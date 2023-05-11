c4102f4f943f40dfbe6b907e512eff02b76e7539
Nicola Muca Cirone <mucacirone.nicola@gmail.com>
2023-05-11 14:39:02 +0100

    Compact NTK

--- src/ControlledResNets.py
+++ src/ControlledResNets.py
@@ -1,17 +1,17 @@
 import torch
 import numpy as np
-
+from collections.abc import Callable
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 class cResNet(torch.nn.Module):
     def __init__(self,
-                 input_channels,
-                 hidden_channels,
-                 phi=lambda x: x,
-                 sigmas=torch.tensor([1.0, 1.0, 1.0]),
-                 partition=torch.linspace(0, 1, 51)):
+                 input_channels: int,
+                 hidden_channels: int,
+                 activation: Callable[[float], float] = lambda x: x,
+                 sigmas: dict[str, float] = {"sigma_0": 1.0, "sigma_A": 1.0, "sigma_b": 0.0},
+                 partition: torch.Tensor = torch.linspace(0, 1, 51)):
 
         super(cResNet, self).__init__()
 
@@ -20,14 +20,15 @@ class cResNet(torch.nn.Module):
         self.hidden_channels = hidden_channels
         N, d = self.hidden_channels, self.input_channels
 
-        self.phi = phi
-        self.std_0, self.std_A, self.std_b = sigmas[0], sigmas[1], sigmas[2]
+        self.phi = activation
+        self.std_0 = sigmas["sigma_0"]
+        self.std_A = sigmas["sigma_A"]
+        self.std_b = sigmas["sigma_b"]
 
         self.partition = partition
         self.M = self.partition.size()[0]
 
         # Sample parameters initial value (z0), matrices (Aks), biases (bks) and projector (v).
-
         self.v_param = torch.nn.Parameter(torch.normal(0.0, 1, size=(1, N)).to(device))
         self.z0_param = torch.nn.Parameter(torch.normal(0.0, 1, size=(N, 1)).to(device))
         self.Aks_param = torch.nn.Parameter(torch.normal(0.0, 1, size=(d, N, N)).to(device))
@@ -39,11 +40,11 @@ class cResNet(torch.nn.Module):
 
                 Parameters
                 ----------
-                X : torch.tensor of dim (batch, timesteps_x, d)
+                X : torch.tensor (batch, timesteps_x, d)
 
                 Returns
                 -------
-                { < v^N, Y_1^{N,D}(x) > }_{x in X} : torch.tensor of dim (batch, 1)
+                { < v^N, Y_1^{N,D}(x) > }_{x in X} : torch.tensor (batch, 1)
         """
 
         batch, times, d = X.shape
@@ -54,7 +55,9 @@ class cResNet(torch.nn.Module):
         z0 = self.std_0 * self.z0_param
         Aks = self.std_A / np.sqrt(self.hidden_channels) * self.Aks_param
         bks = self.std_b * self.bks_param
-        fields = torch.cat([Aks, bks], dim=-1)  # Concatenation of the Aks and bks
+        # Concatenation of the Aks and bks
+        # fields: (d, N, N+1)
+        fields = torch.cat([Aks, bks], dim=-1)
 
         diff_X = X.diff(dim=1)  # Batched increments : (batch, timesteps_x - 1, d)
 
@@ -68,7 +71,7 @@ class cResNet(torch.nn.Module):
             z = self.phi(Y)  # Y ->  \phi(Y)
             z = torch.cat((z, torch.ones([batch, 1, 1]).to(device)), 1)  # z -> [z,1]
             z = z.unsqueeze(1)  # z : (batch, N+1, 1) -> z : (batch, 1, N+1, 1)
-            G = fields.expand(1, -1, -1, -1)  # fields : (d, N, N+1) ->  G : (1, d, N, N+1)
+            G = fields.unsqueeze(0)  # fields : (d, N, N+1) ->  G : (1, d, N, N+1)
             res = torch.matmul(G, z)  # res : (batch, d, N, 1)
             res = res.squeeze(-1)  # res : (batch, d, N, 1) ->  res : (batch, d, N)
             res = torch.swapaxes(res, -2, -1)  # res : (batch, d, N) -> res : (batch, N, d)
